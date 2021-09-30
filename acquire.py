e3a839ac5eccbac1f3784e058e108589030d528c
lopezelihezer <lopezelihezer@gmail.com>
2021-09-30 17:00:35 -0500

    working on acquiring data via api

---
+++ acquire.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import requests
+
+def gather_info(url):
+    response = requests.get(url)
+    print(response.ok)
+    print(response.status_code)
+    print(response.text)
+    return data = response.json()
+
